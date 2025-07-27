040fd3a8df0b431a32ab41147242a03ad465046f
Aryan Singh <137031264+gambare2@users.noreply.github.com>
2025-07-27 17:59:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors';
 import connectDB from './db/index.js'
 import dotenv from 'dotenv';
+import { adminAuth } from './firebase.js';
 
 dotenv.config({
     path: './.env'
@@ -22,6 +23,17 @@ connectDB()
 app.post('/register', (req, res) => {
     console.log('Received data:', req.body);
     res.status(200).json({ message: 'Registration successful' });
+});
+
+
+app.post('/verifyToken', async (req, res) => {
+  const { idToken } = req.body;
+  try {
+    const decoded = await adminAuth.verifyIdToken(idToken);
+    res.json({ uid: decoded.uid });
+  } catch (err) {
+    res.status(401).send('Invalid token');
+  }
 });
 
 
