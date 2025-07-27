098991b0b2d57ed8ff2cb56b8c90bd17c5b62c74
Aryan Singh <137031264+gambare2@users.noreply.github.com>
2025-07-27 17:54:31 +0530

    Update Firebase.js

--- backend/Firebase.js
+++ backend/Firebase.js
@@ -1,22 +1,9 @@
-import { initializeApp } from "firebase-admin";
-import {getAuth, GithubAuthProvider, GoogleAuthProvider} from 'firebase/auth';
-
-
-const firebaseconfig  = {
-  apiKey: "AIzaSyDUQRHkdBV3RInbSmN7h54ZvvRST2Ufl2g",
-  authDomain: "login-page-ebd72.firebaseapp.com",
-  projectId: "login-page-ebd72",
-  storageBucket: "login-page-ebd72.firebasestorage.app",
-  messagingSenderId: "51163584118",
-  appId: "1:51163584118:web:59a21ef0c13f37a2cd7157",
-  measurementId: "G-NEDYJV8NMW"
-};
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
+import serviceAccount from './serviceAccountKey.json' assert { type: "json" }; // if using ESM
 
 initializeApp({
-    credential: applicationDefault(firebaseconfig),
-})
+  credential: cert(serviceAccount)
+});
 
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const githubProvider = new GithubAuthProvider();
-
+export const adminAuth = getAuth();
